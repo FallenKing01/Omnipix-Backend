d04926f3b4bdbe8c7b07e3c02dc1508471f6c784
AluculeseiAndrei <133212017+FallenKing01@users.noreply.github.com>
2024-03-12 13:45:59 +0200

    Bug fix

--- Application/Services/departamentService.py
+++ Application/Services/departamentService.py
@@ -4,6 +4,7 @@ from Infrastructure.Repositories.skillXdepartamentRepo import *
 from Infrastructure.Repositories.SkillRepo import getSkillByIdRepo
 from Application.Services.organizationServices import getOrganizationService
 from Application.Services.userServices import getUserByIdService
+from Infrastructure.Repositories.DepartamentRepo import getDepartmentByIdRepo
 def getDepartamentManagerByEmployeeIdService(id):
 
     isManager = getDepartamentManagerByEmployeeIdRepo(id)
